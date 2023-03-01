4710b1c8f9e31c1fbbeb8fc69e39bf3052aee10e
Platon <platon.solovev05@gmail.com>
2023-03-01 21:58:40 +0300

    TwoSum

--- Easy/TwoSum/Solution.py
+++ Easy/TwoSum/Solution.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def twoSum(self, nums, target):
+    def twoSum(nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
@@ -7,8 +7,7 @@ class Solution(object):
 
 
 if __name__ == "__main__":
-    solution = Solution()
     print("Example 1: {}\nExample 2: {}\nExample 3: {}".format(
-        solution.twoSum([2, 7, 11, 15], 9),
-        solution.twoSum([3, 2, 4], 6),
-        solution.twoSum([3, 3], 6)))
+        Solution.twoSum([2, 7, 11, 15], 9),
+        Solution.twoSum([3, 2, 4], 6),
+        Solution.twoSum([3, 3], 6)))
