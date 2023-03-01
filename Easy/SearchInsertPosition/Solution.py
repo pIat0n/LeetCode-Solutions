59fdcd3434da51c69fc7c4ef9fd6de2368e367de
Platon <platon.solovev05@gmail.com>
2023-03-01 21:57:57 +0300

    SearchInsertPosition

--- Easy/SearchInsertPosition/Solution.py
+++ Easy/SearchInsertPosition/Solution.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def searchInsert(self, nums, target):
+    def searchInsert(nums, target):
         lo = 0
         hi = len(nums)
         while lo < hi:
@@ -12,8 +12,7 @@ class Solution(object):
 
 
 if __name__ == "__main__":
-    solution = Solution()
     print("Example 1: {}\nExample 2: {}\nExample 3: {}".format(
-        solution.searchInsert([1, 3, 5, 6], 5),
-        solution.searchInsert([1, 3, 5, 6], 2),
-        solution.searchInsert([1, 3, 5, 6], 7)))
+        Solution.searchInsert([1, 3, 5, 6], 5),
+        Solution.searchInsert([1, 3, 5, 6], 2),
+        Solution.searchInsert([1, 3, 5, 6], 7)))
