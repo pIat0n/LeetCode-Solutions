5f31445c82ec3f6792d4adcf801918a1f40df90e
Platon <platon.solovev05@gmail.com>
2023-03-01 21:54:41 +0300

    RemoveDuplicatesFromSortedArray

--- Easy/RemoveDuplicatesFromSortedArray/Solution.py
+++ Easy/RemoveDuplicatesFromSortedArray/Solution.py
@@ -1,11 +1,10 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
+    def removeDuplicates(nums):
         nums[:] = sorted(set(nums))
         return len(nums)
 
 
 if __name__ == "__main__":
-    solution = Solution()
     print("Example 1: {}\nExample 2: {}".format(
-        solution.removeDuplicates([1, 1, 2]),
-        solution.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])))
+        Solution.removeDuplicates([1, 1, 2]),
+        Solution.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])))
