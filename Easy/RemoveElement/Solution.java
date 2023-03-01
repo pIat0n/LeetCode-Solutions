16c8d8051e88701573ac086aabba764763ad8f7d
Platon <platon.solovev05@gmail.com>
2023-03-01 21:46:49 +0300

    RemoveElement

--- Easy/RemoveElement/Solution.java
+++ Easy/RemoveElement/Solution.java
@@ -1,6 +1,6 @@
 package Easy.RemoveElement;
 
-public class Solution {
+public class Solution{
     public static int removeElement(int[] nums, int val){
         int i = 0;
         for (int num = 0; num < nums.length; num++) if (nums[num] != val) nums[i++] = nums[num];
