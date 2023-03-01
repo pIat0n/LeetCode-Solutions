993b5e634605af07f373b13a41260d478486165d
Platon <platon.solovev05@gmail.com>
2023-03-01 21:56:36 +0300

    RemoveElement

--- Easy/RemoveElement/Solution.py
+++ Easy/RemoveElement/Solution.py
@@ -1,12 +1,11 @@
 class Solution(object):
-    def removeElement(self, nums, val):
+    def removeElement(nums, val):
         while val in nums:
             nums.remove(val)
         return len(nums)
 
 
 if __name__ == "__main__":
-    solution = Solution()
     print("Example 1: {}\nExample 2: {}".format(
-        solution.removeElement([3, 2, 2, 3], 3),
-        solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)))
+        Solution.removeElement([3, 2, 2, 3], 3),
+        Solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)))
