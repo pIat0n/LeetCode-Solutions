0acc5657c31743a70f83dee7c6e7d98d06c6cb50
Platon <platon.solovev05@gmail.com>
2023-03-09 21:59:23 +0300

    AddBinary

--- Easy/AddBinary/SolutionTest.py
+++ Easy/AddBinary/SolutionTest.py
@@ -1,9 +1,11 @@
 from unittest import TestCase, main
 from Solution import Solution
 
-class AddBinaryTest(TestCase):
+class SolutionTest(TestCase):
     def test_addBinary(self):
         self.assertEqual("100", Solution.addBinary("11", "1"))
+
+
     def test_addBinary2(self):
         self.assertEqual("10101", Solution.addBinary("1010", "1011"))
 
