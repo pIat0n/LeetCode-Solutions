eadc2dcc7d9ecda14dacbe74aefcc8a4779dbd63
Platon <platon.solovev05@gmail.com>
2023-03-09 23:12:35 +0300

    RemoveElement

--- Easy/RemoveElement/Solution.java
+++ Easy/RemoveElement/Solution.java
@@ -6,10 +6,4 @@ public class Solution{
         for (int num = 0; num < nums.length; num++) if (nums[num] != val) nums[i++] = nums[num];
         return i;
     }
-
-    public static void main(String[] args){
-        System.out.printf("Example 1: %d\nExanple 2: %d",
-            Solution.removeElement(new int[]{3, 2, 2, 3}, 3),
-            Solution.removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2));
-    }
 }
--- Easy/RemoveElement/Solution.py
+++ Easy/RemoveElement/Solution.py
@@ -3,9 +3,3 @@ class Solution:
         while val in nums:
             nums.remove(val)
         return len(nums)
-
-
-if __name__ == "__main__":
-    print("Example 1: {}\nExample 2: {}".format(
-        Solution.removeElement([3, 2, 2, 3], 3),
-        Solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)))
