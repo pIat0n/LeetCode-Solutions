b9adea377ee61057c60703f64f42a9225b55baee
Platon <platon.solovev05@gmail.com>
2023-03-09 23:19:10 +0300

    RomanToInteger

--- Easy/RomanToInteger/Solution.java
+++ Easy/RomanToInteger/Solution.java
@@ -3,7 +3,7 @@ package Easy.RomanToInteger;
 import java.util.HashMap;
 
 public class Solution{
-    public static int romanToInt(String s){
+    public int romanToInt(String s){
         HashMap<Character, Integer> roman = new HashMap<>();
         roman.put('I', 1);roman.put('V', 5);roman.put('X', 10);roman.put('L', 50);roman.put('C', 100);roman.put('D', 500);roman.put('M', 1000);
         int count = 0;
--- Easy/RomanToInteger/Solution.py
+++ Easy/RomanToInteger/Solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def romanToInt(s):
+    def romanToInt(self, s):
         roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
         count = 0
         for i in range(len(s) - 1):
