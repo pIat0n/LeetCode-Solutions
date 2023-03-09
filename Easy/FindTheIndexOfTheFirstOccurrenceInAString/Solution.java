2faca8091123d976c510a23846f645a7e660283d
Platon <platon.solovev05@gmail.com>
2023-03-09 21:31:57 +0300

    FindTheIndexOfTheFirstOccurrenceInAString

--- Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
+++ Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
@@ -1,6 +1,6 @@
 package Easy.FindTheIndexOfTheFirstOccurrenceInAString;
 
-public class Solution {
+public class Solution{
     public static int strStr(String haystack, String needle){
         return haystack.indexOf(needle);
     }
