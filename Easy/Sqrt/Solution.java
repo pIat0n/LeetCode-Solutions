a633de4b6baf80146b29435674924791974b73e0
Platon <platon.solovev05@gmail.com>
2023-03-09 13:14:28 +0300

    Sqrt

---
+++ Easy/Sqrt/Solution.java
@@ -0,0 +1,12 @@
+package Easy.Sqrt;
+
+public class Solution {
+    public static int mySqrt(int x){
+        return (int) Math.pow(x, 0.5);
+    }
+    public static void main(String[] args){
+        System.out.printf("Example 1: %d\nExample 2: %d",
+            Solution.mySqrt(4),
+            Solution.mySqrt(8));
+    }
+}
---
+++ Easy/Sqrt/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(x):
+        return int(x ** 0.5)
+
+
+if __name__ == "__main__":
+    print("Example 1: {}\nExample 2: {}".format(
+        Solution.mySqrt(4),
+        Solution.mySqrt(8)))
