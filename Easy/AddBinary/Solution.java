d00a8b7e760fee5bcef06e38eb0bbe18c865d497
Platon <platon.solovev05@gmail.com>
2023-03-09 23:24:54 +0300

    AddBinary

--- Easy/AddBinary/Solution.java
+++ Easy/AddBinary/Solution.java
@@ -3,7 +3,7 @@ package Easy.AddBinary;
 import java.math.BigInteger;
 
 public class Solution{
-    public static String addBinary(String a, String b){
+    public String addBinary(String a, String b){
         return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
     }
 }
