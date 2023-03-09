090913b6fbb028a6b5dd67ee92fc4ee80c306701
Platon <platon.solovev05@gmail.com>
2023-03-09 23:11:01 +0300

    TwoSum

--- Easy/TwoSum/Solution.java
+++ Easy/TwoSum/Solution.java
@@ -1,7 +1,5 @@
 package Easy.TwoSum;
 
-import java.util.Arrays;
-
 public class Solution{
     public static int[] twoSum(int[] nums, int target){
         for (int i = 0; i < nums.length; i++){
@@ -10,12 +8,5 @@ public class Solution{
             }
         }
         return new int[]{};
-    }
-
-    public static void main(String[] args){
-        System.out.printf("Example 1: %s\nExample 2: %s\nExample 3: %s",
-            Arrays.toString(Solution.twoSum(new int[]{2, 7, 11, 15}, 9)),
-            Arrays.toString(Solution.twoSum(new int[]{3, 2, 4}, 6)),
-            Arrays.toString(Solution.twoSum(new int[]{3, 3}, 6)));
     }
 }
--- Easy/TwoSum/Solution.py
+++ Easy/TwoSum/Solution.py
@@ -4,10 +4,3 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-
-
-if __name__ == "__main__":
-    print("Example 1: {}\nExample 2: {}\nExample 3: {}".format(
-        Solution.twoSum([2, 7, 11, 15], 9),
-        Solution.twoSum([3, 2, 4], 6),
-        Solution.twoSum([3, 3], 6)))
