53803b8160a3ea966eeef6411d9a8e88ad30065e
Platon <platon.solovev05@gmail.com>
2023-03-09 21:21:36 +0300

    AddBinary

--- Easy/AddBinary/Solution.java
+++ Easy/AddBinary/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
         return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.printf("Example 1: %s\nExample 2: %s",
             Solution.addBinary("11", "1"),
             Solution.addBinary("1010", "1011"));
