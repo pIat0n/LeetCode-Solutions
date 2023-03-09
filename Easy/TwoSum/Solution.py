0f3c1e5df668488c749ead327bd0d93fd62ec97a
Platon <platon.solovev05@gmail.com>
2023-03-09 23:17:37 +0300

    TwoSum

--- Easy/TwoSum/Solution.py
+++ Easy/TwoSum/Solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(nums, target):
+    def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
