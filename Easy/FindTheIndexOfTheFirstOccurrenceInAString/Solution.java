a537888f300fcbce677c7da9556eb33fb858eeb8
Platon <platon.solovev05@gmail.com>
2023-03-09 23:24:03 +0300

    FindTheIndexOfTheFirstOccurrenceInAString

--- Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
+++ Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
@@ -1,7 +1,7 @@
 package Easy.FindTheIndexOfTheFirstOccurrenceInAString;
 
 public class Solution{
-    public static int strStr(String haystack, String needle){
+    public int strStr(String haystack, String needle){
         return haystack.indexOf(needle);
     }
 }
--- Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.py
+++ Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.py
@@ -1,3 +1,3 @@
 class Solution:
-    def strStr(haystack, needle):
+    def strStr(self, haystack, needle):
         return haystack.find(needle)
