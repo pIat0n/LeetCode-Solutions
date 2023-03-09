31055fadad43f5aef65247123985ca27d32eb057
Platon <platon.solovev05@gmail.com>
2023-03-09 23:14:09 +0300

    PalindromeNumber

--- Easy/PalindromeNumber/Solution.py
+++ Easy/PalindromeNumber/Solution.py
@@ -1,10 +1,3 @@
 class Solution:
     def isPalindrome(x):
         return str(x) == str(x)[::-1]
-
-
-if __name__ == "__main__":
-    print("Example 1: {}\nExample 2: {}\nExample 3: {}".format(
-        Solution.isPalindrome(121),
-        Solution.isPalindrome(-121),
-        Solution.isPalindrome(10)))
