d678543edf767912d460d72fb82fa20200c58ace
Platon <platon.solovev05@gmail.com>
2023-03-09 21:31:31 +0300

    AddBinary

--- Easy/AddBinary/Solution.java
+++ Easy/AddBinary/Solution.java
@@ -2,12 +2,12 @@ package Easy.AddBinary;
 
 import java.math.BigInteger;
 
-public class Solution {
+public class Solution{
     public static String addBinary(String a, String b){
         return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.printf("Example 1: %s\nExample 2: %s",
             Solution.addBinary("11", "1"),
             Solution.addBinary("1010", "1011"));
