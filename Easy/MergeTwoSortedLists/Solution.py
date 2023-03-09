5abb69df01bea126a9b93b85f77d3f6f90437527
Platon <platon.solovev05@gmail.com>
2023-03-09 23:22:30 +0300

    MergeTwoSortedLists

--- Easy/MergeTwoSortedLists/Solution.py
+++ Easy/MergeTwoSortedLists/Solution.py
@@ -5,12 +5,12 @@ class ListNode:
 
 
 class Solution:
-    def mergeTwoLists(list1, list2):
+    def mergeTwoLists(self, list1, list2):
         if list1 == None: return list2
         if list2 == None: return list1
         if list1.val < list2.val:
-            list1.next = Solution.mergeTwoLists(list1.next, list2)
+            list1.next = self.mergeTwoLists(list1.next, list2)
             return list1
         else:
-            list2.next = Solution.mergeTwoLists(list1, list2.next)
+            list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
