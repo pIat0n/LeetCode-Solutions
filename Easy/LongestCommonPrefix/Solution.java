3ae0e362c7ac0c323a4534b12f6a3efc1268caa3
Platon <platon.solovev05@gmail.com>
2023-03-09 23:23:03 +0300

    LongestCommonPrefix

--- Easy/LongestCommonPrefix/Solution.java
+++ Easy/LongestCommonPrefix/Solution.java
@@ -1,7 +1,7 @@
 package Easy.LongestCommonPrefix;
 
 public class Solution{
-    public static String longestCommonPrefix(String[] strs){
+    public String longestCommonPrefix(String[] strs){
         for (int i = 0; i < strs[0].length(); i++){
             for (int c = 1; c < strs.length; c++){
                 if (i == strs[c].length() || strs[c].charAt(i) != strs[0].charAt(i)) return strs[0].substring(0, i);
--- Easy/LongestCommonPrefix/Solution.py
+++ Easy/LongestCommonPrefix/Solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestCommonPrefix(strs):
+    def longestCommonPrefix(self, strs):
         for i, c in enumerate(min(strs)):
             if c != max(strs)[i]:
                 return min(strs)[:i]
