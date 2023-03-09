06e95608936c743ddb38dbb44a7a9668595d1b6e
Platon <platon.solovev05@gmail.com>
2023-03-09 23:20:10 +0300

    RemoveDuplicatesFromSortedArray

--- Easy/RemoveDuplicatesFromSortedArray/Solution.java
+++ Easy/RemoveDuplicatesFromSortedArray/Solution.java
@@ -3,7 +3,7 @@ package Easy.RemoveDuplicatesFromSortedArray;
 import java.util.LinkedHashSet;
 
 public class Solution{
-    public static int removeDuplicates(int[] nums){
+    public int removeDuplicates(int[] nums){
         LinkedHashSet<Integer> set = new LinkedHashSet<>();
         for (int num: nums) set.add(num);
         int i = 0;
--- Easy/RemoveDuplicatesFromSortedArray/Solution.py
+++ Easy/RemoveDuplicatesFromSortedArray/Solution.py
@@ -1,4 +1,4 @@
 class Solution:
-    def removeDuplicates(nums):
+    def removeDuplicates(self, nums):
         nums[:] = sorted(set(nums))
         return len(nums)
