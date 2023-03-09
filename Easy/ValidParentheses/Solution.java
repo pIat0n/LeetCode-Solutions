90e38f0e893fa9473d14df381100d285876ec15c
Platon <platon.solovev05@gmail.com>
2023-03-09 21:28:55 +0300

    ValidParentheses

--- Easy/ValidParentheses/Solution.java
+++ Easy/ValidParentheses/Solution.java
@@ -1,7 +1,7 @@
 package Easy.ValidParentheses;
 
-public class Solution{
-    public static boolean isValid(String s){
+public class Solution {
+    public static boolean isValid(String s) {
         while (s.contains("()") || s.contains("{}") || s.contains("[]")){
             s = s.replace("()", "");
             s = s.replace("{}", "");
@@ -10,7 +10,7 @@ public class Solution{
         return s.isEmpty();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.printf("Example 1: %b\nExample 2: %b\nExample 3: %b",
             Solution.isValid("()"),
             Solution.isValid("()[]{}"),
