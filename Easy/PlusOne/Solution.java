a1b3f1d87ca4566326b6c11dd4f34e6cbfdfa035
Platon <platon.solovev05@gmail.com>
2023-03-09 23:13:33 +0300

    PlusOne

--- Easy/PlusOne/Solution.java
+++ Easy/PlusOne/Solution.java
@@ -1,7 +1,5 @@
 package Easy.PlusOne;
 
-import java.util.Arrays;
-
 public class Solution{
     public static int[] plusOne(int[] digits){
         for (int i = digits.length - 1; i >= 0; i--){
@@ -11,12 +9,5 @@ public class Solution{
         int[] res = new int[digits.length + 1];
         res[0] = 1;
         return res;
-    }
-
-    public static void main(String[] args){
-        System.out.printf("Example 1: %s\nExample 2: %s\nExample 3: %s",
-            Arrays.toString(Solution.plusOne(new int[]{1, 2, 3})),
-            Arrays.toString(Solution.plusOne(new int[]{4, 3, 2, 1})),
-            Arrays.toString(Solution.plusOne(new int[]{9})));
     }
 }
--- Easy/PlusOne/Solution.py
+++ Easy/PlusOne/Solution.py
@@ -2,10 +2,3 @@ class Solution:
     def plusOne(digits):
         a = str(int("".join(map(str, digits))) + 1)
         return [int(i) for i in a]
-
-
-if __name__ == "__main__":
-    print("Example 1: {}\nExample 2: {}\nExample 3: {}".format(
-        Solution.plusOne([1, 2, 3]),
-        Solution.plusOne([4, 3, 2, 1]),
-        Solution.plusOne([9])))
