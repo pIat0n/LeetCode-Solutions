27cfb6d776042d7cb08998a09e53d99f36601430
Platon <platon.solovev05@gmail.com>
2023-03-09 23:27:40 +0300

    AddBinary

--- Easy/AddBinary/SolutionTest.java
+++ Easy/AddBinary/SolutionTest.java
@@ -7,11 +7,11 @@ public class SolutionTest{
 
     @Test
     public void addBinaryTest1(){
-        assertEquals("100", Solution.addBinary("11", "1"));
+        assertEquals("100", new Solution().addBinary("11", "1"));
     }
 
     @Test
     public void addBinaryTest2(){
-        assertEquals("10101", Solution.addBinary("1010", "1011"));
+        assertEquals("10101", new Solution().addBinary("1010", "1011"));
     }
 }
--- Easy/AddBinary/SolutionTest.py
+++ Easy/AddBinary/SolutionTest.py
@@ -3,11 +3,11 @@ from Solution import Solution
 
 class SolutionTest(TestCase):
     def test1_addBinary(self):
-        self.assertEqual("100", Solution.addBinary("11", "1"))
+        self.assertEqual("100", Solution().addBinary("11", "1"))
 
 
     def test2_addBinary(self):
-        self.assertEqual("10101", Solution.addBinary("1010", "1011"))
+        self.assertEqual("10101", Solution().addBinary("1010", "1011"))
 
 
 if __name__ == "__main__":
