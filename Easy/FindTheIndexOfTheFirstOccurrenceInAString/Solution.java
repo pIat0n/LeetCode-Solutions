38edef09eab6c15f7e2acf2e98be6d322cb761fc
Platon <platon.solovev05@gmail.com>
2023-03-10 21:41:37 +0300

    FindTheIndexOfTheFirstOccurrenceInAString

--- Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
+++ Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
@@ -4,4 +4,10 @@ public class Solution{
     public int strStr(String haystack, String needle){
         return haystack.indexOf(needle);
     }
+
+    public static void main(String[] args){
+        System.out.printf("Example 1: %d\nExample 2: %d",
+            new Solution().strStr("sadbutsad", "sad"),
+            new Solution().strStr("leetcode", "leeto"));
+    }
 }
--- Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.py
+++ Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def strStr(self, haystack, needle):
         return haystack.find(needle)
+
+
+if __name__ == "__main__":
+    print("Example 1: {}\nExample 2: {}".format(
+        Solution().strStr("sadbutsad", "sad"),
+        Solution().strStr("leetcode", "leeto")))
