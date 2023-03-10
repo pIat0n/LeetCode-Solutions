a347008701d0be49f725b96faa70c12eb6d96736
Platon <platon.solovev05@gmail.com>
2023-03-10 22:01:54 +0300

    AddBinary

--- Easy/AddBinary/Solution.java
+++ Easy/AddBinary/Solution.java
@@ -6,4 +6,10 @@ public class Solution{
     public String addBinary(String a, String b){
         return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
     }
+
+    public static void main(String[] args){
+        System.out.printf("Example 1: %s\nExample 2: %s",
+            new Solution().addBinary("11", "1"),
+            new Solution().addBinary("1010", "1011"));
+    }
 }
--- Easy/AddBinary/Solution.py
+++ Easy/AddBinary/Solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def addBinary(self, a, b):
         return bin(int(a, 2) + int(b, 2))[2:]
+
+
+if __name__ == "__main__":
+    print("Example 1: {}\nExample 2: {}".format(
+        Solution().addBinary("11", "1"),
+        Solution().addBinary("1010", "1011")))
