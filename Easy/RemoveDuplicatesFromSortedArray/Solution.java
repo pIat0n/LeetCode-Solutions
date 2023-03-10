1ac1be54981b2122ff5573384caa072d89bd4568
Platon <platon.solovev05@gmail.com>
2023-03-10 21:33:45 +0300

    RemoveDuplicatesFromSortedArray

--- Easy/RemoveDuplicatesFromSortedArray/Solution.java
+++ Easy/RemoveDuplicatesFromSortedArray/Solution.java
@@ -10,4 +10,10 @@ public class Solution{
         for (int s: set) nums[i++] = s; 
         return set.size();
     }
+
+    public static void main(String[] args){
+        System.out.printf("Example 1: %d\nExample 2: %d",
+            new Solution().removeDuplicates(new int[]{1, 1, 2}),
+            new Solution().removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
+    }
 }
--- Easy/RemoveDuplicatesFromSortedArray/Solution.py
+++ Easy/RemoveDuplicatesFromSortedArray/Solution.py
@@ -2,3 +2,9 @@ class Solution:
     def removeDuplicates(self, nums):
         nums[:] = sorted(set(nums))
         return len(nums)
+
+
+if __name__ == "__main__":
+    print("Example 1: {}\nExample 2: {}".format(
+        Solution().removeDuplicates([1, 1, 2]),
+        Solution().removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])))
