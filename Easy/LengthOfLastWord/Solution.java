152ecee471c95d3c62c3764cedd57c35604aed50
Platon <platon.solovev05@gmail.com>
2023-03-10 21:54:22 +0300

    LengthOfLastWord

--- Easy/LengthOfLastWord/Solution.java
+++ Easy/LengthOfLastWord/Solution.java
@@ -4,4 +4,11 @@ public class Solution{
     public int lengthOfLastWord(String s){
         return s.split(" ")[s.split(" ").length - 1].length();
     }
+
+    public static void main(String[] args){
+        System.out.printf("example 1: %d\nExample 2: %d\nExample 3: %d",
+            new Solution().lengthOfLastWord("Hello World"),
+            new Solution().lengthOfLastWord("   fly me   to   the moon  "),
+            new Solution().lengthOfLastWord("luffy is still joyboy"));
+    }
 }
--- Easy/LengthOfLastWord/Solution.py
+++ Easy/LengthOfLastWord/Solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s):
         return len(s.split()[-1])
+
+
+if __name__ == "__main__":
+    print("Example 1: {}\nExample 2: {}\nExample 3: {}".format(
+        Solution().lengthOfLastWord("Hello World"),
+        Solution().lengthOfLastWord("   fly me   to   the moon  "),
+        Solution().lengthOfLastWord("luffy is still joyboy")))
