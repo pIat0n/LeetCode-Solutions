998cb45991a21bc6579f5c7fee3ea4f03edc70ef
Platon <platon.solovev05@gmail.com>
2023-03-10 22:03:59 +0300

    Sqrt

--- Easy/Sqrt/Solution.java
+++ Easy/Sqrt/Solution.java
@@ -4,4 +4,10 @@ public class Solution{
     public int mySqrt(int x){
         return (int) Math.pow(x, 0.5);
     }
+
+    public static void main(String[] args){
+        System.out.printf("Example 1: %d\nExample 2: %d",
+            new Solution().mySqrt(4),
+            new Solution().mySqrt(8));
+    }
 }
