ed9a706fbd51297cc928b882c64fcb57b0876f81
Platon <platon.solovev05@gmail.com>
2023-03-10 21:12:26 +0300

    PalindromeNumber

--- Easy/PalindromeNumber/Solution.java
+++ Easy/PalindromeNumber/Solution.java
@@ -4,4 +4,11 @@ public class Solution{
     public boolean isPalindrome(int x){
         return String.valueOf(x).equals(new StringBuilder(String.valueOf(x)).reverse().toString());
     }
+
+    public static void main(String[] args){
+        System.out.printf("Example 1: %b\nExample 2: %b\nExample 3: %b",
+            new Solution().isPalindrome(121),
+            new Solution().isPalindrome(-121),
+            new Solution().isPalindrome(10));
+    }
 }
