d5a0b738c06d9c5023e4e048bc550c54162b48fb
Platon <platon.solovev05@gmail.com>
2023-03-10 21:08:09 +0300

    TwoSum

--- Easy/TwoSum/Solution.java
+++ Easy/TwoSum/Solution.java
@@ -1,5 +1,7 @@
 package Easy.TwoSum;
 
+import java.util.Arrays;
+
 public class Solution{
     public int[] twoSum(int[] nums, int target){
         for (int i = 0; i < nums.length; i++){
@@ -8,5 +10,12 @@ public class Solution{
             }
         }
         return new int[]{};
+    }
+
+    public static void main(String[] args){
+        System.out.printf("Example 1: %s\nExample 2: %s\nExample 3: %s",
+            Arrays.toString(new Solution().twoSum(new int[]{2, 7, 11, 15}, 9)),
+            Arrays.toString(new Solution().twoSum(new int[]{3, 2, 4}, 6)),
+            Arrays.toString(new Solution().twoSum(new int[]{3, 3}, 6)));
     }
 }
