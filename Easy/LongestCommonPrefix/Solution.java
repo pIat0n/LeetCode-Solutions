adefd7e55607fa36013cce85152173cc464ef191
Platon <platon.solovev05@gmail.com>
2023-03-13 17:16:01 +0300

    LongestCommonPrefix

--- Easy/LongestCommonPrefix/Solution.java
+++ Easy/LongestCommonPrefix/Solution.java
@@ -2,7 +2,11 @@ package Easy.LongestCommonPrefix;
 
 public class Solution{
     public String longestCommonPrefix(String[] strs){
-        for (Integer i = 0; i < strs[0].length(); i++) for (int c = 1; c < strs.length; c++) if (i.equals(strs[c].length()) || strs[c].charAt(i) != strs[0].charAt(i)) return strs[0].substring(0, i);
+        for (Integer i = 0; i < strs[0].length(); i++){
+            for (int c = 0; c < strs.length; c++){
+                if (i.equals(strs[c].length()) || strs[c].charAt(i) != strs[0].charAt(i)) return strs[0].substring(0, i);
+            }
+        }
         return strs[0];
     }
 
